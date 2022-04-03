fd553bb5f5ca9c07eceb087ca8947def66d73531
Jonathan Yang <JonPYang@gmail.com>
2022-04-02 23:51:30 -0400

    Create properly working base game
    Computer randomly picks number between 1-3
    1=Rock, 2=Paper, 3=Scissors
    Program asks user for String choice (case insensitive)
    Convert player choice into Int
    Subtract Computer Int from player Int
    Evaluate winner with table
    0 = Tie
    -1 / 2 = Player winner
    1 / -2 = Computer winner

--- javascript.js
+++ javascript.js
@@ -1,1 +1,69 @@
-console.log("Hello World")
+// function computerPlay() {
+//     switch(Math.floor((Math.random() * 3))) {
+//         case 1:
+//             return "Rock"
+//         case 2:
+//             return "paper"
+//         case 3:
+//             return "scissors"
+//     }
+// }
+
+function computerPlay() {
+    return(Math.floor(Math.random() * 3) + 1)
+}
+
+function convert(input) {
+    switch(input) {
+        case 1:
+            return "Rock"
+        case 2:
+            return "Paper"
+        case 3:
+            return "Scissors"
+        case "rock":
+            return 1
+        case "paper":
+            return 2
+        case "scissors":
+            return 3
+    }
+}
+
+function getInput(){
+    let validateInput = true
+    let input = ""
+
+    while(validateInput) {
+        input = prompt("Input Rock, Paper, or Scissors").toLowerCase();
+
+        if(input == "rock" || input == "paper" || input == "scissors") {
+            validateInput = false;
+        } else {
+            alert("Improper selection: Try again.")
+        }
+    }
+    return convert(input)
+}
+
+
+
+function playMatch(playerSelection, computerSelection) {
+
+    let result = playerSelection - computerSelection;
+    let computerChoice = convert(computerSelection)
+
+    if (result == -1 || result == 2) {
+        alert(`You lose! Computer chose ${computerChoice}`)
+    }
+    else if (result == 1 || result == -2) {
+        alert(`You win! Computer chose ${computerChoice}`)
+    }
+    else {
+        alert(`it's a tie! Computer chose ${computerChoice}`)
+    }
+}
+
+for(let x = 0; x < 5; x++) {
+    playMatch(getInput(), computerPlay())
+}
